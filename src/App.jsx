892ebd17a39f4c0f526791744ff8b51667213c60
mohammedyahyakal <98487400+mohammedyahyakal@users.noreply.github.com>
2024-09-16 19:37:03 +0300

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Singin from "./pages/Singin";
+import Singup from "./pages/Singup";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/projects";
 function App() {
-  return <div className="bg-red-900 text-red-700">rwer</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Singin />} />
+        <Route path="/sign-up" element={<Singup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
