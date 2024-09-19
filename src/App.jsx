d56e41f536a29c65ee5199314fef4034d30862a9
mohammedyahyakal <98487400+mohammedyahyakal@users.noreply.github.com>
2024-09-19 22:23:32 +0300

    create and add the footer component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Singup from "./pages/Singup";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/projects";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
